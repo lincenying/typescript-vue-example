759ea249961d97f1e963909083c339e291cc5843
林岑影 <lincenying@qq.com>
2018-09-05 10:03:09 +0800

    no message

--- src/api/config.ts
+++
@@ -1,4 +0,0 @@
-export default {
-    api: 'https://cnodejs.org/api/v1/',
-    timeout: 30000
-}
--- src/types.d.ts
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-
-declare module 'vue/types/vue' {
-    // 3. 声明为 Vue 补充的东西
-    interface Vue {
-        $$from: any
-    }
-}

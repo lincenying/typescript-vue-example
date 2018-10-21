9b79d89f545acd0516e8517f5fa0ad01077f79fc
林岑影 <lincenying@qq.com>
2018-10-21 23:33:07 +0800

    up

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,8 @@ module.exports = {
     baseUrl: process.env.NODE_ENV === 'production' ? '/typescript-vue-example/' : '/',
     configureWebpack: {
         devtool: 'source-map'
+    },
+    devServer: {
+        port: 9090
     }
 }
